4e3a5deee4d77942012832ab658eba82b9a641f4
KabodGarments <kabodgarments@gmail.com>
2019-03-03 22:50:58 +0300

    Added and defined functions some more. Business Logic Arithmetic

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,28 @@ function Player(name, roll, diceOne, diceTwo) {
   this.diceOne = diceOne;
   this.diceTwo = diceTwo;
 }
+
+Player.prototype.bankScore = function () {
+  this.playerOverallScore += this.playerTurnScore;
+}
+
+Player.prototype.rollDice = function () {
+  this.diceOne = Math.floor((Math.random() * 6) + 1)
+  this.diceTwo = Math.floor((Math.random() * 6) + 1)
+  console.log(this.diceOne);
+  console.log(this.diceTwo);
+  this.playerRoll = this.diceOne + this.diceTwo
+  if (this.playerRoll == 2) {
+    this.playerOverallScore = 0;
+    this.playerTurnScore = 0;
+    M.toast({html: 'Bad lack friend! Total score back down to 0...'})
+  } else if (this.diceOne === 1 || this.diceTwo === 1) {
+    this.playerTurnScore = 0;
+    M.toast({html: 'You rolled a 1! Turn over friend.'})
+  } else if (this.diceOne === this.diceTwo) {
+    this.playerTurnScore += (this.playerRoll += this.playerRoll);
+    M.toast({html: 'Matching pair! Double points friend!'})
+  } else {
+    this.playerTurnScore += this.playerRoll;
+  }
+}
