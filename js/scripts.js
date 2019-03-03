c48cfc58e86ec7730323c86104ceeb4fe0ab408d
KabodGarments <kabodgarments@gmail.com>
2019-03-03 22:48:46 +0300

    Added first layer of functions

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function Player(name, roll, diceOne, diceTwo) {
+  this.playerName = name;
+  this.playerTurnScore = 0;
+  this.playerOverallScore = 0;
+  this.playerRoll = roll
+  this.diceOne = diceOne;
+  this.diceTwo = diceTwo;
+}
